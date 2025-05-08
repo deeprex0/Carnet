e45632287f29f8e4935dcafe7c8de0406eb14b76
universe <daibbar.mohamed@gmail.com>
2025-05-08 12:17:26 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using CarnetClasses;
+
+
+
+Console.WriteLine("MR7ba bik fl Carnet Dialk!");
+
+bool eawd = true;
+
+while(eawd){
+
+Console.WriteLine("1. Ettdaya  2. Chddada 3. Khroj");
+Console.Write("Khtar chi Option:");
+
+var Choix =  Convert.ToInt32(Console.ReadLine());
+
+if(Choix == 1){
+
+    var path ="Ettdaya.txt";
+
+    Console.WriteLine("Khtar l operation li bghiti dir");
+    Console.WriteLine("1. Zid ettday");
+    Console.WriteLine("2. N9ass ettday");
+    Console.WriteLine("3. Afficher l ettdaya");
+    Console.WriteLine("4. Qallab 3la ettday");
+    Console.WriteLine("5.Khroj");
+    Console.Write("Choose an option:");
+
+    Choix = Convert.ToInt32(Console.ReadLine());
+
+    switch( Choix){
+        case 1:
+        Ettday.Zid(path, "Ettday");
+        break;
+        case 2: 
+        Console.Write("ara smito: "); var smia = Convert.ToString(Console.ReadLine());
+#pragma warning disable CS8604 // Possible null reference argument.
+        Ettday.Nqass(path, smia);
+#pragma warning restore CS8604 // Possible null reference argument.
+        break;
+        case 3:
+        Ettday.Afficher(path);
+        break;
+        case 4:
+        Console.Write("ara smito: ");
+        var smito = Convert.ToString(Console.ReadLine());
+#pragma warning disable CS8604 // Possible null reference argument.
+        Ettday.Qallab(smito);
+#pragma warning restore CS8604 // Possible null reference argument.
+        break;
+        case 5:
+        eawd = false;
+        break;
+
+    
+
+
+
+
+
+    }
+    
+
+}
+
+else if(Choix == 3){
+    eawd = false;
+}
+
+else{
+    Console.WriteLine("makayn walo ajmi");
+
+}
+
+}
---
+++ Utilities/CarnetClasses.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices.Marshalling;
+using System.Runtime.CompilerServices;
+
+namespace CarnetClasses
+{
+
+        public  abstract class Files
+        {
+            internal static void Zid(string path, string type ){
+
+                Console.Write("Ara smiat had l {type}: "); var smia = Convert.ToString(Console.ReadLine());
+                Console.Write("ach 3tdak: "); var l7aja = Console.ReadLine();
+                Console.Write("ch7al sawi?: "); var tamane = Console.ReadLine();
+
+#pragma warning disable CS8604 // Possible null reference argument.
+            File.AppendAllText(path, StarMaker(smia, l7aja, tamane) + Environment.NewLine);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+
+        }
+
+        internal static List<string> Upload(string path){
+
+            return new List<string>(File.ReadAllLines(path));
+
+           
+        }
+
+         static  void Download(string path, List<string> stora){
+            File.WriteAllLines(path, stora);
+        }
+
+        internal static int Qallab(List<string> lines, string smia){
+            lines.Sort();
+            return  lines.BinarySearch(smia);
+        }
+
+
+        internal static void Nqass(string path, string smia){
+            List<string> stora = Upload(path);
+
+            stora.RemoveAt(Qallab(stora, smia));
+
+            Download(path,stora);
+
+
+
+        }
+
+        internal static void Afficher(string Path){
+            var stora = Upload(Path);
+            foreach ( var star in stora){
+                Console.WriteLine(star + "\n");
+            }
+
+
+        }
+
+        public static string StarMaker(string smia, string l7aja, string prix){
+            return smia + " " + l7aja + " " + prix + "\n";
+        }
+    }
+
+
+    internal  class Ettday : Files{
+
+          internal static  string  Qallab(string smia){
+            List<string> lines = Files.Upload("Ettdaya.txt");
+
+            lines.Sort();
+            int index = lines.BinarySearch(smia);
+
+            return lines[index];
+
+
+        }
+
+
+        
+
+        
+
+    }
+
+
+}
